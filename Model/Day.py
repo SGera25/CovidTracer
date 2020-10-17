dc66b495e9c0f9dc5758aceb44f23c8b5037d739
Sam-Gera <samuel.j.gera@gmail.com>
2020-10-17 13:22:27 -0600

    Updated filenames

--- Model/Day.py
+++ Model/Day.py
@@ -1,1 +1,19 @@
-	
+from TimeBlock import TimeBlock
+
+
+
+
+class Day:
+
+	def __init__(self, Date: str):
+		self.initTimeBlocks()
+
+
+
+	def initTimeBlocks(self):
+		timeBlocks = set()
+		for x in range(96):
+			timeBlocks.add(TimeBlock())
+	def addPerson()
+
+x = Day("Date")
