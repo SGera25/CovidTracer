0ee71d4115502655adda871f7565c500486c990a
SamHCam <samhcam@gmail.com>
2020-10-17 10:44:23 -0600

    Added preliminary login page

--- Model/website.py
+++ Model/website.py
@@ -1,10 +1,22 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 @app.route("/map")
 def map():
